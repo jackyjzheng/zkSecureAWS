545e13badf75ec81cfdd5f20a461e130a14bca69
dennismfong <dfong2014@gmail.com>
2018-02-03 17:12:11 -0800

    Add the lambda function source code

---
+++ implementation/lambda_sourcecode/iot_to_dynamo.py
@@ -0,0 +1,20 @@
+import boto3
+
+def lambda_handler(event, ccontext):
+	dynamodb = boto3.resource('dynamodb') 
+	table = dynamodb.Table(tableName)
+	table.put_item(
+	  Item = {
+	    'deviceId' : event.deviceId,
+	    'timestamp' : event.timestamp,
+	    'data' : {
+	      'ip' : event.data.ip,
+	      'signature' : event.data.signature,
+	      'encryptedData' : event.data.encryptedData,
+	      'tempData' : {
+	        'tempC' : event.data.tempData.tempC,
+	        'tempF' : event.data.tempData.tempF
+	      }
+	    }
+	  }
+	)
