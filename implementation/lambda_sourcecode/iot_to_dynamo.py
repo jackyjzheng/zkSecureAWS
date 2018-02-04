fb01507b0f0e4e49f00d1a35eb1ebfde17daaf6c
dennismfong <dfong2014@gmail.com>
2018-02-03 22:48:44 -0800

    Fixed access of python dict

--- implementation/lambda_sourcecode/iot_to_dynamo.py
+++ implementation/lambda_sourcecode/iot_to_dynamo.py
@@ -1,17 +1,19 @@
+import boto3
+
 def lambda_handler(event, context):
 	dynamodb = boto3.resource('dynamodb') 
-	table = dynamodb.Table(tableName)
+	table = dynamodb.Table('IoT')
 	table.put_item(
 	  Item = {
-	    'deviceId' : event.deviceId,
-	    'timestamp' : event.timestamp,
+	    'deviceId' : event['deviceId'],
+	    'timestamp' : event['timestamp'],
 	    'data' : {
-	      'ip' : event.data.ip,
-	      'signature' : event.data.signature,
-	      'encryptedData' : event.data.encryptedData,
+	      'ip' : event['data']['ip'],
+	      'signature' : event['data']['signature'],
+	      'encryptedData' : event['data']['encryptedData'],
 	      'tempData' : {
-	        'tempC' : event.data.tempData.tempC,
-	        'tempF' : event.data.tempData.tempF
+	        'tempC' : event['data']['tempData']['tempC'],
+	        'tempF' : event['data']['tempData']['tempF']
 	      }
 	    }
 	  }
