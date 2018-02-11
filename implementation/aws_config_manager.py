490841865bf1da0d6cf29d943ba5182a8e4aa75a
dennismfong <dfong2014@gmail.com>
2018-02-11 12:28:23 -0800

    Added setters and save function

--- implementation/aws_config_manager.py
+++ implementation/aws_config_manager.py
@@ -3,7 +3,8 @@ import ConfigParser
 
 class AWS_Config_Manager:
 
-  CONFIG_PATH = os.path.expanduser("~") + '/.aws/zymkeyconfig.ini'
+  CONFIG_PATH = os.path.expanduser("~") + '/.aws/zymkeyconfig'
+  SECTION_NAME = 'aws_arns'
 
   def __init__(self):
     self.config = ConfigParser.ConfigParser()
@@ -13,17 +14,43 @@ class AWS_Config_Manager:
     self.table_name = ''
     self.initializeConfig()
 
+  def __del__(self):
+    self.saveConfig()
+
   def initializeConfig(self):
-    awsSectionName = 'aws_arns'
-    isExisting = os.path.exists(AWS_Config_Manager.CONFIG_PATH)
-    if (isExisting):
-      pass
-      #self.config.read(CONFIG_PATH)
+    fileExists = os.path.exists(AWS_Config_Manager.CONFIG_PATH)
+    if (fileExists):
+      self.config.read(AWS_Config_Manager.CONFIG_PATH)
+      self.role_arn = self.config.get(AWS_Config_Manager.SECTION_NAME, 'role_arn')
+      self.policy_arn = self.config.get(AWS_Config_Manager.SECTION_NAME, 'policy_arn')
+      self.lambda_arn = self.config.get(AWS_Config_Manager.SECTION_NAME, 'lambda_arn')
+      self.table_name = self.config.get(AWS_Config_Manager.SECTION_NAME, 'table_name')
     else:
-      self.config.add_section(awsSectionName)
-      self.config.set(awsSectionName, 'role_arn', '')
-      self.config.set(awsSectionName, 'policy_arn', '')
-      self.config.set(awsSectionName, 'lambda_arn', '')
-      self.config.set(awsSectionName, 'table_name', '')
-      with open(AWS_Config_Manager.CONFIG_PATH, 'w') as configfile:
-        self.config.write(configfile)
+      self.config.add_section(AWS_Config_Manager.SECTION_NAME)
+      self.config.set(AWS_Config_Manager.SECTION_NAME, 'role_arn', '')
+      self.config.set(AWS_Config_Manager.SECTION_NAME, 'policy_arn', '')
+      self.config.set(AWS_Config_Manager.SECTION_NAME, 'lambda_arn', '')
+      self.config.set(AWS_Config_Manager.SECTION_NAME, 'table_name', '')
+      self.saveConfig()
+
+  def saveConfig(self):
+    with open(AWS_Config_Manager.CONFIG_PATH, 'w') as configfile:
+      self.config.write(configfile)
+
+  def setRole(self, roleArn):
+    self.config.set(AWS_Config_Manager.SECTION_NAME, 'role_arn', roleArn)
+    self.role_arn = roleArn
+
+  def setPolicy(self, policyArn):
+    self.config.set(AWS_Config_Manager.SECTION_NAME, 'policy_arn', policyArn)
+    self.policy_arn = policyArn
+
+  def setLambda(self, lambdaArn):
+    self.config.set(AWS_Config_Manager.SECTION_NAME, 'lambda_arn', lambdaArn)
+    self.lambda_arn = lambdaArn
+
+  def setTable(self, tableName):
+    self.config.set(AWS_Config_Manager.SECTION_NAME, 'table_name', tableName)
+    self.table_name = tableName
+
+
