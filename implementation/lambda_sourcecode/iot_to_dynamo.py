7ce95c3b23555d6816a087e6434bc6db1fb3d2e5
dennismfong <dfong2014@gmail.com>
2018-02-11 01:36:16 -0800

    Change lambda function to accept all JSON payloads with timestamp and deviceId

--- implementation/lambda_sourcecode/iot_to_dynamo.py
+++ implementation/lambda_sourcecode/iot_to_dynamo.py
@@ -3,18 +3,5 @@ import boto3
 def lambda_handler(event, context):
 	dynamodb = boto3.resource('dynamodb') 
 	table = dynamodb.Table('IoT')
-	table.put_item(
-	  Item = {
-	    'deviceId' : event['deviceId'],
-	    'timestamp' : event['timestamp'],
-	    'data' : {
-	      'ip' : event['data']['ip'],
-	      'signature' : event['data']['signature'],
-	      'encryptedData' : event['data']['encryptedData'],
-	      'tempData' : {
-	        'tempC' : event['data']['tempData']['tempC'],
-	        'tempF' : event['data']['tempData']['tempF']
-	      }
-	    }
-	  }
-	)
+
+	table.put_item(Item = event)
