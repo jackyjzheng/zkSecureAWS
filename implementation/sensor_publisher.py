3361aebd2663622d1ec3484f7c237184c9cf03e3
dennismfong <dfong2014@gmail.com>
2018-02-24 00:59:11 -0800

    Changing naming convention of changeable variables and setting timeout of pycurl to 2 seconds

--- implementation/sensor_publisher.py
+++ implementation/sensor_publisher.py
@@ -15,23 +15,30 @@ import random
 import datetime
 import zymkey
 
-failQ = Queue()
-qSem = Semaphore()
-rwSem = Semaphore()
+# Global variables for the script
+LOGFILE_NAME = 'log.txt'
+TOPIC = 'Zymkey'
+DEVICE_ID = '1'
+IP = '192.168.12.28'
+DATA_SEP = '---NEW ITEM---'
+FAIL_THRESHOLD = 100
 
+failQ = Queue() # The queue of JSON objects that failed to upload
+qSem = Semaphore() # The semaphore for putting and getting from the queue
+rwSem = Semaphore() # The semaphore for reading and writing from the log
+
+# Create a log.txt file if it doesn't already exist
 cur_dir = os.path.dirname(os.path.realpath(__file__))
-log_path = os.path.join(cur_dir, 'log.txt')
+log_path = os.path.join(cur_dir, LOGFILE_NAME)
 if not os.path.isfile(log_path):
   f = open(log_path,"w+")
   f.close()
 
-# Data generation setup
+# Variables to setup the AWS endpoint for publishing data
 boto3client = boto3.client('iot')
-topic = "Zymkey"
-AWS_ENDPOINT = "https://" + str(boto3client.describe_endpoint()['endpointAddress']) + ":8443/topics/" + topic + "?qos=1"    
-device_id = "1"
-ip = "192.168.12.28"
+AWS_ENDPOINT = "https://" + str(boto3client.describe_endpoint()['endpointAddress']) + ":8443/topics/" + TOPIC + "?qos=1"    
 
+# Function using pycurl to upload data to the topic 
 def ZK_AWS_Publish(url, post_field, CA_Path, Cert_Path,):
   #Setting Curl to use zymkey_ssl engine
   c = pycurl.Curl()
@@ -57,14 +64,14 @@ def ZK_AWS_Publish(url, post_field, CA_Path, Cert_Path,):
   c.setopt(c.VERBOSE, 0)
   c.setopt(c.SSLKEYTYPE, "ENG") 
   c.setopt(c.SSLKEY, "nonzymkey.key")
-  c.setopt(c.TIMEOUT, 5)
+  c.setopt(c.TIMEOUT, 2)
   try:
     c.perform()
     return 0
   except Exception as e:
     return -1
 
-# Checking if we can connect to one of Google's IP
+# Checking if we can connect to one of Google's IP to check if our internet connection is up
 def internet_on():
     try:
         urllib2.urlopen('http://216.58.192.142', timeout=2)
@@ -72,23 +79,23 @@ def internet_on():
     except urllib2.URLError as err: 
         return False
     except Exception as e:
-      print('no')
+      print(e)
 
 # This thread would check for any data failed to publish from the failQ queue and write it to a log file
 def checkFailQueue():
   global internetOn
   while True:
     qSem.acquire()
-    if failQ.qsize() > 100 or (internetOn and (failQ.qsize() is not 0)):
+    if failQ.qsize() > FAIL_THRESHOLD or (internetOn and (failQ.qsize() is not 0)):
       print('Queue has reached size ' + str(failQ.qsize()))
       rwSem.acquire()
       with open(log_path, "a") as myfile:
-        num = 0
+        numObjects = 0
         while failQ.qsize() > 0:
           data = failQ.get()
-          myfile.write('---NEW ITEM---\n' + data + '\n')
-          num += 1
-        print('Wrote ' + str(num) + ' items from queue to log')
+          myfile.write(DATA_SEP + '\n' + data + '\n') # Separate each data object by a characteristic line
+          numObjects += 1
+        print('Wrote ' + str(numObjects) + ' items from queue to log')
       rwSem.release() 
     qSem.release()
 
@@ -101,20 +108,20 @@ def retrySend():
       if not os.stat(log_path).st_size == 0: # There is data that needs to reupload
         numPublish = 1
         with open(log_path) as f:
-          next(f) # Skip the first ---NEW ITEM--- tag
+          next(f) # Skip the first DATA_SEP tag
           dataBuilder = ''
           json_data = ''
           for line in f:
-            line.rstrip()
-            if '---NEW ITEM---' not in line:
-              dataBuilder += line
+            line.rstrip() # Strip newline characters
+            if DATA_SEP not in line:
+              dataBuilder += line # Build up the JSON payload line by line of file
             else:
-              json_data = dataBuilder
+              json_data = dataBuilder # Reached the data separator string so now we store dataBuilder as the json data
               print('RETRY ITEM ' + str(numPublish) + ': ' + json_data)
               if ZK_AWS_Publish(url=AWS_ENDPOINT, post_field=json_data, CA_Path='/home/pi/Zymkey-AWS-Kit/bash_scripts/CA_files/zk_ca.pem', Cert_Path='/home/pi/Zymkey-AWS-Kit/zymkey.crt') is not -1:
                 print('\tRETRY PUBLISH item ' + str(numPublish) + ' from retry\n')
               else:
-                print('Couldnt publish ' + str(numPublish) + ' added to queue')
+                print('Couldnt publish ' + str(numPublish) + ', added to queue')
                 failQ.put(json_data)
               numPublish += 1
               dataBuilder = '' # Reset the dataBuilder to empty string
@@ -149,8 +156,8 @@ try:
     temp_data = {"tempF": random.randint(70,100), "tempC" : random.randint(35, 50)}
     encrypted_data = zymkey.client.lock(bytearray(json.dumps(temp_data)))
     signature = zymkey.client.sign(encrypted_data)
-    data = {"ip": ip, "signature": binascii.hexlify(signature), "encryptedData": binascii.hexlify(encrypted_data), "tempData": temp_data}
-    post_field = {"deviceId": device_id, "timestamp": str(timestamp), "data": data}
+    data = {"ip": IP, "signature": binascii.hexlify(signature), "encryptedData": binascii.hexlify(encrypted_data), "tempData": temp_data}
+    post_field = {"deviceId": DEVICE_ID, "timestamp": str(timestamp), "data": data}
     json_data = json.dumps(post_field)
 
     if not internet_on():
