18a7b313e8ba3eeb0fea0acc1bf85a02d2f74250
Jacky Zheng <jzheng1126@yahoo.com>
2018-03-08 13:32:36 -0800

    removed redundant script.

--- bash_scripts/install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo apt-get update
-sudo apt-get install OpenSSL
-sudo apt-get install libcurl4-openssl-dev libcurl4-openssl-dev
-sudo apt-get install python-openssl
-
-pip install boto3
-pip install pycurl
